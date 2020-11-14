287b85163d5f04c8eaaaa235c93479919f48261b
James Morgan O <MorganJay@users.noreply.github.com>
2020-11-14 19:18:56 +0100

    Add project files.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        //Public identifiers
+        //Private
+        //Internal
+        //Protected
+        //Sealed
+        //Abstract
+        //HackerRank
+        //LeetCode, Codility, Codewar
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐿", "🐿",
+                "🦎","🦎",
+                "🐱", "🐱",
+                "🤡", "🤡",
+                "🐪", "🐪",
+                "🐵", "🐵",
+                "🦄", "🦄",
+                "🐦", "🐦",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
